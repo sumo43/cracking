836f56ed2d29456c23081a1660196194798b06f7
yatsenkoa <yatsenkoa2020@gmail.com>
2021-05-27 13:03:49 -0400

    working on 7

---
+++ 6/10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void simulation(void) {
+    vector<int> soda(1000, 0);
+    //pretend this is random
+    soda[234] = 1;
+    int strips = 10;
+    int days = 0;
+
+    for(int i = 0; i < strips; i++) {
+        
+
+
+
+int main(void) {
+
+
+}
+
